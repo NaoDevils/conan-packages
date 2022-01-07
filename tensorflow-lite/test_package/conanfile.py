80ea303176bde516c4e452c4b0dcd205cac59154
Aaron Larisch <aaron.larisch@tu-dortmund.de>
2022-01-07 08:46:00 +0100

    Fix test run environment

--- tensorflow-lite/test_package/conanfile.py
+++ tensorflow-lite/test_package/conanfile.py
@@ -1,26 +1,17 @@
+from conans import ConanFile, CMake, tools
 import os
 
-from conans import ConanFile, tools
-from conan.tools.cmake import CMake
-from conan.tools.layout import cmake_layout
 
-
-class TensorflowLiteTestConan(ConanFile):
+class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    # VirtualBuildEnv and VirtualRunEnv can be avoided if "tools.env.virtualenv:auto_use" is defined
-    # (it will be defined in Conan 2.0)
-    generators = "CMakeDeps", "CMakeToolchain", "VirtualBuildEnv", "VirtualRunEnv"
-    apply_env = False
+    generators = "cmake"
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
-    def layout(self):
-        cmake_layout(self)
-
     def test(self):
         if not tools.cross_building(self):
-            cmd = os.path.join(self.cpp.build.bindirs[0], "example")
-            self.run(cmd, env="conanrunenv")
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
