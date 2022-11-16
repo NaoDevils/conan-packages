0e89d831356367e334bed92e72b6f65c04c13ff6
Aaron Larisch <aaron.larisch@tu-dortmund.de>
2022-11-16 09:32:46 +0100

    Fix debug build

--- libmpc/conanfile.py
+++ libmpc/conanfile.py
@@ -27,6 +27,7 @@ class LibmpcConan(ConanFile):
     def _configure_cmake(self):
         if not self._cmake:
             self._cmake = CMake(self)
+            self._cmake.definitions["CMAKE_SKIP_INSTALL_ALL_DEPENDENCY"] = True
             self._cmake.configure(source_folder=self._source_subfolder, build_folder=self._build_subfolder)
         return self._cmake
         
@@ -37,14 +38,21 @@ class LibmpcConan(ConanFile):
     def source(self):
         tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
 
-    def build(self):
-        cmake = self._configure_cmake()
-        cmake.build()
+    # Since it's a header-only library and the tests CMakeLists.txt does not interact 
+    # with Conan properly, we just skip the build step here and install directly.
+    # def build(self):
+        # cmake = self._configure_cmake()
+        # cmake.build()
         
     def requirements(self):
         self.requires("eigen/3.4.0")
         self.requires("nlopt/2.7.1")
         self.requires("osqp/0.6.2")
+        
+    def build_requirements(self):
+        self.build_requires("ninja/1.11.0")
+        if self.settings.build_type == "Debug":
+            self.build_requires("catch2/3.1.0")
 
     def package(self):
         cmake = self._configure_cmake()
